8eac8e9282b6cd44d8aefecef4e47fe80956a770
Spencer <spencer@frozor.io>
2019-04-02 16:49:35 -0400

    Tests for all operators

--- tests/helper/babel.js
+++ tests/helper/babel.js
@@ -1,6 +1,10 @@
 const babel = require('babel-core');
 const { default: plugin } = require('../../dist/babel-plugin');
 
-module.exports = function babelHelper(source) {
+module.exports = function babelHelper(source, noPlugins = false) {
+    if (noPlugins) {
+        return babel.transform(source).code;
+    }
+
     return babel.transform(source, { plugins: [[plugin, { skipImport: true }]] }).code;
 };
--- tests/unit/babel-transformer.test.js
+++ tests/unit/babel-transformer.test.js
@@ -2,43 +2,158 @@ const { expect } = require('chai');
 const transformCode = require('../helper/babel');
 
 function expectNoTransformation(source) {
-   expect(transformCode(source)).to.equal(source);
+   // The second call to transform without plugins is done because sometimes babel strips "useless"
+   // stuff out of the source code, such as left-hand parens for addition (don't ask me why).
+   // Thus, we compare it to a transformation that occurs with no plugins enabled, so that we know
+   // that any potential transformations are the work of Babel, and not us.
+   expect(transformCode(source)).to.equal(transformCode(source, true));
 }
 
-function expectTransformation(source, desiredOutput) {
+function expectSpecificTransformation(source, desiredOutput) {
    expect(transformCode(source)).to.equal(desiredOutput);
 }
 
-describe('babel transformer', function () {
-   describe('Binary Expressions', function () {
-      // TODO: Run this in a for loop across all supported binary operators to ensure that behavior is consistent
-      describe('addition', function () {
-         describe('numeric literal + other', function () {
-            it('does not perform any transformations when other is a numeric literal', function () {
-               expectNoTransformation('1 + 2;');
-            });
+function expectAnyTransformation(source) {
+   expect(transformCode(source)).to.not.equal(transformCode(source, true));
+}
 
-            it('does not perform any transformations for nested numeric literal operations', function () {
-               // TODO: Fix this test!!!
-               expectNoTransformation('1 + (2 + (3 + (4 + (5 + 6 + 7 + 8 + (9 + 10)))));');
-            });
-
-            it('generates the operator when other is not a numeric literal', function () {
-               expectTransformation('1 + a;', 'Operator.add(1, a);');
-            });
+function binaryExpressionTests(symbol, methodName) {
+   describe(`a ${symbol} b [${methodName}]`, function () {
+      describe(`numeric literal ${symbol} other`, function () {
+         it('does not perform any transformations when other is a numeric literal', function () {
+            expectNoTransformation(`1 ${symbol} 2;`);
          });
 
-         describe('non-NumericLiteral + other', function () {
-            it('generates the operator when other is a')
+         it('generates the operator when other is not a numeric literal', function () {
+            expectSpecificTransformation(`1 ${symbol} a`, `Operator.${methodName}(1, a);`);
+         });
+
+         describe('nested expressions', function () {
+            it('does not transform when all variables are numeric literals, on the right side', function () {
+               expectNoTransformation(`1 ${symbol} (2 ${symbol} (3 ${symbol} (4 ${symbol} (5 ${symbol} 6 ${symbol} 7 ${symbol} 8 ${symbol} (9 ${symbol} 10)))));`);
+            });
+
+            it('does not transform when all variables are numeric literals, on the left side', function () {
+               expectNoTransformation(`(((((9 ${symbol} 10) ${symbol} 8 ${symbol} 7 ${symbol} 6 ${symbol} 5) ${symbol} 4) ${symbol} 3) ${symbol} 2) ${symbol} 1;`);
+            });
+         });
+      });
+
+      describe(`non numeric literal ${symbol} other`, function () {
+         it(`generates the operator when both are identifiers`, function () {
+            expectSpecificTransformation(`a ${symbol} b`, `Operator.${methodName}(a, b);`);
+         });
+
+         it(`generates the operator when either one is a member expression`, function () {
+            expectSpecificTransformation(`cat.a ${symbol} b`, `Operator.${methodName}(cat.a, b);`);
+            expectSpecificTransformation(`a ${symbol} cat.b`, `Operator.${methodName}(a, cat.b);`);
+            expectSpecificTransformation(`cat.a ${symbol} cat.b`, `Operator.${methodName}(cat.a, cat.b);`);
          });
       });
    });
+}
 
-   describe('Assignment Expressions', function () {
+function assignmentExpressionTests(symbol, methodName) {
+   describe(`a ${symbol}= b [${methodName}]`, function () {
+      it(`transforms when performing ${methodName} on a numeric literal to an identifier`, function () {
+         expectSpecificTransformation(`a ${symbol}= 2`, `a = Operator.${methodName}(a, 2);`);
+      });
 
+      it(`transforms when performing ${methodName} on a numeric literal to a member expression`, function () {
+         expectSpecificTransformation(`cat.a ${symbol}= 2`, `cat.a = Operator.${methodName}(cat.a, 2);`);
+      });
+
+      it(`transforms when performing ${methodName} on a binary expression of an identifier to an identifier`, function () {
+         expectSpecificTransformation(`a ${symbol}= (b ${symbol} 5)`, `a = Operator.${methodName}(a, Operator.${methodName}(b, 5));`);
+      });
+   });
+}
+
+function updateExpressionTests(symbol, methodName) {
+   describe(`${symbol}a [prefix ${methodName}]`, function () {
+      it('transforms when on its own', function () {
+         expectSpecificTransformation(`${symbol}a`, `a = Operator.${methodName}(a);`);
+      });
+
+      it('transforms when assigning to a value', function () {
+         expectSpecificTransformation(`a = ${symbol}b`, `a = b = Operator.${methodName}(b);`);
+      });
    });
 
-   describe('Unary Expressions', function () {
+   describe(`a${symbol} [postfix ${methodName}]`, function () {
+      it('transforms when on its own', function () {
+         expectSpecificTransformation(`a${symbol}`, `(_temp = a) && ((a = Operator.${methodName}(a)) || _temp) && _temp;`);
+      });
 
+      it('transforms when assigning to a value', function () {
+         expectSpecificTransformation(`a = b${symbol}`, `a = (_temp = b) && ((b = Operator.${methodName}(b)) || _temp) && _temp;`);
+      });
    });
+}
+
+function unaryExpressionTests(symbol, methodName) {
+   describe(`${symbol}a [${methodName}]`, function () {
+      it('does not transform types that are not identifiers/member expressions', function () {
+         expectNoTransformation(`${symbol}2`);
+         expectNoTransformation(`${symbol}[]`);
+         expectNoTransformation(`${symbol}true`);
+         expectNoTransformation(`${symbol}'cat'`);
+         expectAnyTransformation(`${symbol}a`);
+         expectAnyTransformation(`${symbol}cat.a`);
+      });
+   });
+}
+
+const assignmentOperatorTestCaseData = [
+   ['+', 'add'],
+   ['-', 'subtract'],
+   ['*', 'multiply'],
+   ['%', 'mod'],
+   ['/', 'divide'],
+   ['**', 'pow'],
+   ['&', 'logicalAnd'],
+   ['|', 'logicalOr'],
+   ['^', 'logicalXor'],
+   ['<<', 'leftShift'],
+   ['>>', 'rightShift']
+];
+
+const binaryOperatorTestCaseData = [
+   ...assignmentOperatorTestCaseData,
+   ['==', 'unsafeEqual'],
+   ['!=', 'unsafeNotEqual'],
+   ['===', 'strictEqual'],
+   ['!==', 'strictNotEqual'],
+   ['<', 'lessThan'],
+   ['>', 'greaterThan'],
+   ['<=', 'lessEqual'],
+   ['>=', 'greaterEqual']
+];
+
+const updateOperatorTestCaseData = [
+   ['++', 'increment'],
+   ['--', 'decrement']
+];
+
+const unaryOperatorTestCaseData = [
+   ['-', 'negate'],
+   ['+', 'positive'],
+   ['~', 'logicalNot'],
+   ['!', 'not']
+];
+
+function runOperatorTests(test, tests) {
+   for (const [symbol, methodName] of tests) {
+      test(symbol, methodName);
+   }
+}
+
+describe('babel transformer', function () {
+   describe('Binary Expressions', () => runOperatorTests(binaryExpressionTests, binaryOperatorTestCaseData));
+
+   describe('Assignment Expressions', () => runOperatorTests(assignmentExpressionTests, assignmentOperatorTestCaseData));
+
+   describe('Unary Expressions', () => runOperatorTests(unaryExpressionTests, unaryOperatorTestCaseData));
+
+   describe('Update Expressions', () => runOperatorTests(updateExpressionTests, updateOperatorTestCaseData));
 });

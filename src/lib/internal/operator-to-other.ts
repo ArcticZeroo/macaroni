89d4dfe6dfe170990080cea84967d20489545ca3
Spencer <spencer@frozor.io>
2019-04-02 16:57:00 -0400

    All supported operators should now have a name

--- src/lib/internal/operator-to-other.ts
+++ src/lib/internal/operator-to-other.ts
@@ -4,7 +4,25 @@ const operatorDisplays: {} = {
     [operators.add]: '+',
     [operators.multiply]: '*',
     [operators.subtract]: '-',
-    [operators.divide]: '/'
+    [operators.divide]: '/',
+    [operators.pow]: '**',
+    [operators.lessThan]: '<',
+    [operators.lessEqual]: '<=',
+    [operators.greaterThan]: '>',
+    [operators.greaterEqual]: '>=',
+    [operators.unsafeEqual]: '==',
+    [operators.strictEqual]: '===',
+    [operators.logicalAnd]: '&',
+    [operators.logicalOr]: '|',
+    [operators.logicalXor]: '^',
+    [operators.logicalNot]: '~',
+    [operators.leftShift]: '<<',
+    [operators.rightShift]: '>>',
+    [operators.increment]: '++',
+    [operators.decrement]: '--',
+    [operators.negate]: '-x',
+    [operators.positive]: '+x',
+    [operators.not]: '!'
 };
 
 export { operatorDisplays };
